b5ee1e6d71352be45affe07e1f80e9fb4d66471d
netlaoer <netlaoer@foxmail.com>
2026-06-02 14:32:28 +0800

    Reapply "输出改目标的目标施法,治疗看着坦克职业就行"
    This reverts commit b1121695f1fb2794fcd37c3f14f0bc9ce84665db.

--- class/Paladin.lua
+++ class/Paladin.lua
@@ -1,6 +1,16 @@
 if UnitClassBase("player") ~= "PALADIN" then return end
 
--- 仅神圣专精添加驱散开关像素块
+-- 仅神圣专精添加扩展像素块
 if Fuyutsui.ClassBlocks and Fuyutsui.ClassBlocks[1] then
     Fuyutsui.ClassBlocks[1][48] = { type = "block", name = "驱散开关" }
+    Fuyutsui.ClassBlocks[1][49] = { type = "block", name = "5码敌人" }
+end
+
+-- 覆盖 MacrosList：进攻技能施法目标
+if GetSpecialization() == 1 then
+    if Fuyutsui.MacrosList and Fuyutsui.MacrosList.staticSpells then
+        Fuyutsui.MacrosList.staticSpells[6] = "[@targettarget,harm,nodead][harm,nodead]审判"
+        Fuyutsui.MacrosList.staticSpells[10] = "[@player]正义盾击"
+        Fuyutsui.MacrosList.staticSpells[14] = "[@targettarget,harm,nodead][harm,nodead]神圣震击"
+    end
 end
